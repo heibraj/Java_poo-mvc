bf9e481e353d155a998fb4f62cd906d407676382
heibraj <heibrajanperez09@gmail.com>
2025-03-25 19:53:42 -0500

    Add files via upload

--- vista/vista.java
+++ vista/vista.java
@@ -1,38 +1,20 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package vista;
 
 /**
  *
- * @author SENA
+ * @author Usuario
  */
 public class vista {
-    public void mostrarBienvenida(){
-        System.out.println("bienvenido al sistema de reservas");
-        System.out.println("vamos a ver los asientos disponibles");
+     public void mostrarResultado(double resultado) {
+        System.out.println("Resultado: " + resultado);
     }
-    public void mostrarEstadoCine(String estado){
-        System.out.println("\nEstado actual de las sillas");
-        System.out.println(estado);
-    }
-    public void mostrarErrorReserva(String mensaje){
+
+    public void mostrarMensaje(String mensaje) {
         System.out.println(mensaje);
     }
-    public void mostrarOpciones(){
-        System.out.println("seleccione una opcion");
-        System.out.println("1:ver el estado de la silla");
-        System.out.println("2:hacer una reserva");
-        System.out.println("3:salir");
-    }
-    public void mostrarMensajeSalida(){
-        System.out.println("\nGracias buen dia");
-    }
-    public void mostrarMensajeCapacidadCompleta(){
-        System.out.println("no hay asientos disponibles");
-    }
-    public void nostrarReservasUsuario(String reservas){
-        System.out.println(reservas);
-    }
-    public void preguntarSiContinuar(){
-        System.out.println("deseas hacer otra reserva");
-    }
+
 }
